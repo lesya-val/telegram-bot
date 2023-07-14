a8bbcbee0b2a74ac357354df2974e8605081190d
lesya-val <you@example.com>
2023-07-14 18:19:46 +0600

    feat: add a command handler /start and create a start button

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import telebot
+from telebot import types
 
 token = '6306041543:AAFHxK1lULkkc54M2ddZwodaJHvXfqN5eO0'
 bot = telebot.TeleBot(token)
@@ -6,8 +7,12 @@ bot = telebot.TeleBot(token)
 
 # Start
 @bot.message_handler(commands=['start'])
-def start_message(message):
-    bot.send_message(message.chat.id, "Привет!")
+def start(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item = types.KeyboardButton('Начать')
+    markup.row(item)
+
+    bot.send_message(message.chat.id, "Привет! Нажми 'Начать' для продолжения.", reply_markup=markup)
 
 
 bot.infinity_polling()
